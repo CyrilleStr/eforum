1506362ef6978103104f2c07ceb92c342af83da6
Cyrille <cyrille.stroesser@orange.fr>
2021-03-23 16:09:04 +0100

    cool

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,6 +7,8 @@ use App\Entity\Post;
 use App\Entity\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,16 +17,22 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title',TextType::class,[
+                'label' => " "
+            ])
             ->add('category',EntityType::class,[
+                'label' => "Categorie",
                 'class' => Category::class,
                 'choice_label' => 'name'
             ])
             ->add('type',EntityType::class,[
+                'label' => "Type",
                 'class' => Type::class,
                 'choice_label' => 'name'
             ])
-            ->add('description')
+            ->add('description',TextareaType::class,[
+                'label' => " "
+            ])
         ;
     }
 
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +15,24 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('activity',TextareaType::class)
-            ->add('password',PasswordType::class)
-            ->add('confirm_password',PasswordType::class)
+            ->add('firstName',TextType::class,[
+                'label' =>"Prénom"
+            ])
+            ->add('lastName',TextType::class,[
+                'label' =>"Nom de famille"
+            ])
+            ->add('email',TextType::class,[
+                'label' =>"Prénom"
+            ])
+            ->add('activity',TextareaType::class,[
+                'label' =>"Activité"
+            ])
+            ->add('password',PasswordType::class,[
+                'label' =>"Mot de passe"
+            ])
+            ->add('confirm_password',PasswordType::class,[
+                'label' =>"Confirmez le mot de passe"
+            ])
         ;
     }
 
